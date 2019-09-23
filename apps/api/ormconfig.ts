efef0e510bccbe60d382d2c92926e7b62e7096a3
Danny Eck <danny@Dannys-MacBook-Air.local>
2019-09-23 16:37:15 -0700

    wip

---
+++ apps/api/ormconfig.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import { ConnectionOptions } from 'typeorm';
+
+console.log('hello world');
+process.exit(1);
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber'
+  }
+};
+
+export default config;
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,33 +1,92 @@
 import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 import * as express from 'express';
 import { Request, Response } from 'express';
 import * as bodyParser from 'body-parser';
 import { Message, IHero } from '@heros-inc/api-interfaces';
+import { Routes } from './routes';
+import { User } from './entity/User';
 
-const app = express();
-app.use(bodyParser.json());
+createConnection()
+  .then(async connection => {
+    // create express app
+    const app = express();
+    app.use(bodyParser.json());
 
-//
-// Routes
-//
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+      (app as any)[route.method](
+        route.route,
+        (req: Request, res: Response, next: Function) => {
+          const result = new (route.controller as any)()[route.action](
+            req,
+            res,
+            next
+          );
+          if (result instanceof Promise) {
+            result.then(result =>
+              result !== null && result !== undefined
+                ? res.send(result)
+                : undefined
+            );
+          } else if (result !== null && result !== undefined) {
+            res.json(result);
+          }
+        }
+      );
+    });
 
-const greeting: Message = { message: 'Hello world!' };
+    // setup express app here
+    // ...
 
-app.get('/', (req, res) => {
-  res.send(greeting);
-});
+    // start express server
+    app.listen(process.env.port || 3333);
 
-app.get('/heroes', (req: Request, res: Response) => {
-  const payload: IHero[] = [
-    { id: '1', name: 'Superman', skills: [], vips: [] },
-    { id: '2', name: 'Batman', skills: [], vips: [] },
-    { id: '3', name: 'IronMan', skills: [], vips: [] }
-  ];
-  return res.send(payload);
-});
+    // insert new users for test
+    await connection.manager.save(
+      connection.manager.create(User, {
+        firstName: 'Timber',
+        lastName: 'Saw',
+        age: 27
+      })
+    );
+    await connection.manager.save(
+      connection.manager.create(User, {
+        firstName: 'Phantom',
+        lastName: 'Assassin',
+        age: 24
+      })
+    );
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log('Listening at http://localhost:' + port + '/api');
-});
-server.on('error', console.error);
+    // console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+    console.log('up :)');
+  })
+  .catch(error => console.log(error));
+
+// const app = express();
+// app.use(bodyParser.json());
+
+// //
+// // Routes
+// //
+
+// const greeting: Message = { message: 'Hello world!' };
+
+// app.get('/', (req, res) => {
+//   res.send(greeting);
+// });
+
+// app.get('/heroes', (req: Request, res: Response) => {
+//   const payload: IHero[] = [
+//     { id: '1', name: 'Superman', skills: [], vips: [] },
+//     { id: '2', name: 'Batman', skills: [], vips: [] },
+//     { id: '3', name: 'IronMan', skills: [], vips: [] }
+//   ];
+//   return res.send(payload);
+// });
+
+// const port = process.env.port || 3333;
+// const server = app.listen(port, () => {
+//   console.log('Listening at http://localhost:' + port + '/api');
+// });
+// server.on('error', console.error);
